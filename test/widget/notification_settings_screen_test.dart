36473a6910df55c8acd50c9cf8064ba3238089dd
KevinTheRainmaker <sweetrainforyou@gmail.com>
2026-03-08 22:25:39 +0900

    chore: remove unused imports from test files

--- test/widget/notification_settings_screen_test.dart
+++ test/widget/notification_settings_screen_test.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:habit_rabbit/domain/entities/notification_settings.dart';
 import 'package:habit_rabbit/presentation/providers/notification_provider.dart';
 import 'package:habit_rabbit/presentation/screens/notification_settings_screen.dart';
 
